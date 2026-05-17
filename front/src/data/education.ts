0515b8ca0bce7d887c5d0ed7d686ffd7bce7ef8a
Vladislav Torgovtsev <vlad4990@gmail.com>
2026-05-17 18:21:17 +0300

    task 3

---
+++ front/src/data/education.ts
@@ -0,0 +1,15 @@
+export type EducationEntry = {
+  degree: string;
+  school: string;
+};
+
+export const education: readonly EducationEntry[] = [
+  {
+    degree: 'Художник-мастер, педагог.',
+    school: 'Колледж декоративно-прикладного искусства им. Карла Фаберже',
+  },
+  {
+    degree: 'Монументальная живопись',
+    school: 'РГУ ИМ. А.Н.КОСЫГИНА, Институт искусств',
+  },
+];
